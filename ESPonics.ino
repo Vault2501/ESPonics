5110b3c7b47d91abbff8e01f615ffc6bccd40466
Christian Hueller <chuller@suse.com>
2024-05-07 15:45:45 +0200

    Remove OTA as it does not work

--- ESPonics.ino
+++ ESPonics.ino
@@ -2,7 +2,6 @@
 #define WEBSERVER_H
 #include <ESPAsyncWebServer.h>
 #include <Wire.h>
-#include <ArduinoOTA.h>
 #include <TaskScheduler.h>
 #include <UnitecRCSwitch.h>
 #include <DHT.h>
@@ -622,53 +621,6 @@ void readSensors() {
   }
 }
 
-//////////////////////////////////////////////
-/// OTA part
-
-void setupOTA() {
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname(myhostname);
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_SPIFFS
-      type = "filesystem";
-    }
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    //SPIFFS.end();
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-}
-
 
 //////////////////////////////////////////////
 /// Main part
@@ -733,11 +685,10 @@ void setup() {
   //ledcAttach(fanpwm1_pin, freq, resolution);
   //ledcAttachChannel(fanpwm1_pin, freq, resolution, fanChannel);
 
-  // setup WiFiManager and OTA updates
+  // setup WiFiManager
   setupWiFiManager();
-  setupOTA();
 
-  // configure preferences strcture for persistent saving
+  // configure preferences structure for persistent saving
   preferences.begin("garden", true);
   spray_period = preferences.getULong("spray_period", 10000);
   spray_duration = preferences.getULong("spray_duration", 1000);
@@ -788,5 +739,5 @@ void loop() {
   setLightState();
   readSensors();
 
-  notifyClients();
+  //notifyClients();
 }
